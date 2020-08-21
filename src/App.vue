7a0113c78d9758645965eb7f889c0c40dbb42759
mzhaonanq <1002604003@qq.com>
2020-08-21 20:48:03 +0800

    创建三个页面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
+
 <template>
   <div id="app">
+    <router-view />
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/money">Money</router-link>|
+      <router-link to="/statistics">Statistics</router-link>|
+      <router-link to="/labels">Labels</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
---
+++ src/views/Statistics.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>我是数据</div>
+</template>
