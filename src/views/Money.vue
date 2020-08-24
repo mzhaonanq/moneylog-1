6f760b5b09dbc42461d1113a49dfcc2d62de6a45
mzhaonanq <1002604003@qq.com>
2020-08-24 20:29:32 +0800

    把record的复制保存至recordList

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad @update:value="onUpdateAmount" />
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord" />
     <Types :value='record.type' @update:value="onUpdateType" />
     <Notes @update:value="onUpdateNotes" />
     <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
@@ -15,7 +15,7 @@ import NumberPad from "@/components/money/NumberPad.vue";
 import Types from "@/components/money/Types.vue";
 import Notes from "@/components/money/Notes.vue";
 import Tags from "@/components/money/Tags.vue";
-import { Component } from "vue-property-decorator";
+import { Component, Watch } from "vue-property-decorator";
 import { component } from "vue/types/umd";
 
 type Record = {
@@ -35,7 +35,7 @@ type Record = {
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
   record: Record = { tags: [], notes: "", type: "-", amount: 0 };
-
+  recordList: Record[] = [];
   onUpdateTags(value: string[]) {
     this.record.tags = value;
   }
@@ -48,7 +48,16 @@ export default class Money extends Vue {
   onUpdateAmount(value: string) {
     this.record.amount = parseFloat(value);
   }
-}
+  saveRecord(){
+    const record2 = JSON.parse(JSON.stringify(this.record)) 
+    this.recordList.push(record2)
+  }
+  @Watch('recordList')
+    onRecordListChange(){
+      window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+    }
+  }
+
 </script>
 
 <style lang="scss">
