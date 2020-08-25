749f44e3b033f79b790e119d72f14357a5cea81e
mzhaonanq <1002604003@qq.com>
2020-08-25 23:51:59 +0800

    可以编辑和删除标签

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -2,31 +2,63 @@ const localStorageKeyName = "tagList";
 type Tag = {
   id: string;
   name: string;
-}
+};
 type TagListModel = {
   data: Tag[];
   fetch: () => Tag[];
-  create: (name: string) => 'success'|'duplicated';
+  create: (name: string) => "success" | "duplicated";
+  update: (id: string, name: string) => "success" | "duplicate" | "not found";
+  remove: (id: string) => boolean;
   save: () => void;
-}
+};
 const tagListModel: TagListModel = {
-  data:[],
+  data: [],
   fetch() {
-    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") 
+    this.data = JSON.parse(
+      window.localStorage.getItem(localStorageKeyName) || "[]"
+    );
     return this.data;
   },
-  create(name: string){ 
-    const names = this.data.map(item=>item.name)
-    if(names.indexOf(name)>=0){return "duplicated"}
-    this.data.push({id:name,name:name})
-    this.save()
-    return "success"
+  create(name: string) {
+    const names = this.data.map((item) => item.name);
+    if (names.indexOf(name) >= 0) {
+      return "duplicated";
+    }
+    this.data.push({ id: name, name: name });
+    this.save();
+    return "success";
+  },
+  update(id, name) {
+    const idList = this.data.map((item) => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.data.map((item) => item.name);
+      if (names.indexOf(name) >= 0) {
+        return "duplicate";
+      } else {
+        const tag = this.data.filter((item) => item.id === id)[0];
+        tag.name = name;
+        tag.id=name;
+        this.save();
+        return "success";
+      }
+    } else {
+      return "not found";
+    }
+  },
+  remove(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.data.splice(index, 1);
+    this.save();
+    return true;
   },
   save() {
-    window.localStorage.setItem(
-      localStorageKeyName,
-      JSON.stringify(this.data)
-    );
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   },
 };
-export default tagListModel
+export default tagListModel;
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,14 +1,19 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icons class="leftIcon" name="left" />
+      <Icons class="leftIcon" name="left" @click.native="goBack" />
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <Notes field-name="标签名" placeholder="请输入标签名" />
+      <Notes
+        :value="tag.name"
+        @update:value="update"
+        field-name="标签名"
+        placeholder="请输入标签名"
+      />
     </div>
-    <div class="button-wrapper"><Button>删除标签</Button></div>
+    <div @click="remove" class="button-wrapper"><Button>删除标签</Button></div>
   </Layout>
 </template>
 
@@ -25,16 +30,30 @@ import Button from "../components/Button.vue";
   },
 })
 export default class EditLabel extends Vue {
+  tag?: { id: string; name: string } = undefined;
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter((t) => t.id === id)[0];
     if (tag) {
-      console.log(tag);
+      this.tag = tag;
     } else {
       this.$router.replace("/404");
     }
+  }
+  update(name: string) {
+    if (this.tag) {
+      tagListModel.update(this.tag.id, name);
+    }
+  }
+  remove(){
+      if(this.tag){
+          tagListModel.remove(this.tag.id)
+      }
+  }
+  goBack(){
+      this.$router.back()
   }
 }
 </script>
@@ -63,9 +82,9 @@ export default class EditLabel extends Vue {
   background: white;
   margin-top: 8px;
 }
-.button-wrapper{
-    text-align: center;
-    padding: 16px;
-    margin-top: 44-16px;
+.button-wrapper {
+  text-align: center;
+  padding: 16px;
+  margin-top: 44-16px;
 }
 </style>
