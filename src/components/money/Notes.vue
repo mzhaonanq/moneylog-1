64a05c3c0c603fc000cdf513f06ad4f92f70a758
mzhaonanq <1002604003@qq.com>
2020-08-25 22:24:42 +0800

    修复了Notes中value在内部被赋值的问题

--- src/components/money/Notes.vue
+++ src/components/money/Notes.vue
@@ -2,7 +2,7 @@
   <div>
     <label class="notes">
       <span class="name">{{this.fieldName}}</span>
-      <input type="text" v-model="value" :placeholder="this.placeholder" />
+      <input type="text" :value="value" @input="onValueChanged($event.target.value)" :placeholder="this.placeholder" />
     </label>
   </div>
 </template>
@@ -12,11 +12,11 @@ import Vue from "vue";
 import { Component, Watch, Prop } from "vue-property-decorator";
 @Component
 export default class Notes extends Vue {
-  value = "";
+  @Prop({default:''}) value!: string;
   @Prop({required:true}) fieldName!: string;
   @Prop()placeholder?: string
   @Watch('value')
-  $onValueChanged(value: string){
+  onValueChanged(value: string){
     this.$emit('update:value',value)
   }
 }
