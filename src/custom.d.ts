7e19cf2dd481d2946b5a57795480243d5ec0359b
mzhaonanq <1002604003@qq.com>
2020-08-26 16:05:40 +0800

    封装tagList的增删改查

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,24 +1,30 @@
 type RecordItem = {
-    tags: string[];
-    notes: string;
-    type: string;
-    amount: number;
-    recordTime?: Date; 
-  };
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+  recordTime?: Date;
+};
 
-  type Tag = {
-    id: string;
-    name: string;
-  };
-  type TagListModel = {
-    data: Tag[];
-    fetch: () => Tag[];
-    create: (name: string) => "success" | "duplicated";
-    update: (id: string, name: string) => "success" | "duplicate" | "not found";
-    remove: (id: string) => boolean;
-    save: () => void;
-  };
-  interface Window {
-    tagList: Tag[];
-    createTag: (name: string) => void;
-  }
+type Tag = {
+  id: string;
+  name: string;
+};
+type TagListModel = {
+  data: Tag[];
+  fetch: () => Tag[];
+  create: (name: string) => "success" | "duplicated";
+  update: (id: string, name: string) => "success" | "duplicate" | "not found";
+  remove: (id: string) => boolean;
+  save: () => void;
+};
+interface Window {
+  tagList: Tag[];
+  createTag: (name: string) => void;
+  removeTag: (id: string) => boolean;
+  updateTag: (
+    id: string,
+    name: string
+  ) => "success" | "duplicate" | "not found";
+  findTag: (id: string) => Tag;
+}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import store from "./store";
 import Nav from "@/components/Nav.vue";
 import Icons from "@/components/Icons.vue";
 import Layout from "@/components/Layout.vue";
-import tagListModel from '@/models/tagListModel.ts';
+import tagListModel from "@/models/tagListModel.ts";
 
 Vue.config.productionTip = false;
 
@@ -14,19 +14,25 @@ Vue.component("Nav", Nav);
 Vue.component("Layout", Layout);
 Vue.component("Icons", Icons);
 
-window.tagList = tagListModel .fetch()
-window.createTag=(name: string)=>{
-  
- 
-    const result = tagListModel.create(name);
-    if(result==='duplicated'){
-      window.alert('标签名重复啦 ')
-    }else if(result==='success'){
-      window.alert('添加成功')
-    }
- 
+window.tagList = tagListModel.fetch();
+window.createTag = (name: string) => {
+  const result = tagListModel.create(name);
+  if (result === "duplicated") {
+    window.alert("标签名重复啦 ");
+  } else if (result === "success") {
+    window.alert("添加成功");
+  }
+};
+window.removeTag=(id: string)=>{
+  return tagListModel.remove(id)
 }
-new Vue({
+window.updateTag=(id: string,name: string)=>{
+  return tagListModel.update(id, name);
+}
+window.findTag=(id: string)=>{
+return  window.tagList.filter((t) => t.id === id)[0];
+}
+ new Vue({
   router: router,
   store,
   render: (h) => h(App),
