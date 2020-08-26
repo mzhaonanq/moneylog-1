88d009c8a208bb4222c32f552b18ec08c6aae36d
mzhaonanq <1002604003@qq.com>
2020-08-26 13:57:50 +0800

    重新封装recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,15 +1,17 @@
 const localStorageKeyName = "recordList";
+
 const recordListModel = {
+  data: [] as RecordItem[], 
     clone(data: RecordItem[]|RecordItem){
         return JSON.parse(JSON.stringify(data)) 
     },
   fetch() {
     return JSON.parse(window.localStorage.getItem(localStorageKeyName) || "[]") as RecordItem[]
   },
-  save(data: RecordItem[]) {
+  save() {
     window.localStorage.setItem(
       localStorageKeyName,
-      JSON.stringify(data)
+      JSON.stringify(this.data)
     );
   },
 };
