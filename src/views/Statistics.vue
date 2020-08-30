20fe3ed172457dfaaf23ceb0d34e95d7af22f2eb
honghuada <1002604003@qq.com>
2020-08-31 00:08:12 +0800

    对statistics.vue的收入和支出如果没有数据，增加了提示语

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -5,22 +5,24 @@
       :data-source="recordTypeList"
       :value.sync="type"
     />
-    <div>
-      <ol>
-        <li v-for="(group,index) in groupedList" :key="index">
-          <h3 class="title">{{beautify(group.title)}}<span>￥{{group.total}}</span></h3>
+   
+      <ol v-if="groupedList.length>0">
+        <li v-for="(group, index) in groupedList" :key="index">
+          <h3 class="title">
+            {{ beautify(group.title) }}<span>￥{{ group.total }}</span>
+          </h3>
           <ol>
-            <li v-for="item in group.items" :key="item.id"
-            class="record"
-            >
-              <span>{{tagString(item.tags)}}</span>
-              <span class="notes">{{item.notes}}</span>
-              <span>￥{{ item.amount }}</span> 
+            <li v-for="item in group.items" :key="item.id" class="record">
+              <span>{{ tagString(item.tags) }}</span>
+              <span class="notes">{{ item.notes }}</span>
+              <span>￥{{ item.amount }}</span>
             </li>
           </ol>
         </li>
       </ol>
-    </div>
+   <div v-else class="noResult">
+     目前没有相关记录
+   </div>
   </Layout>
 </template>
 
@@ -30,8 +32,8 @@ import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
 import recordTypeList from "@/constants/recordTypeList.ts";
 import store from "@/store";
-import dayjs from "dayjs"
-import clone from '@/lib/clone';
+import dayjs from "dayjs";
+import clone from "@/lib/clone";
 
 @Component({
   components: {
@@ -48,52 +50,66 @@ export default class Statistics extends Vue {
     return (this.$store.state as RootState).recordList;
   }
   get groupedList() {
-      const {recordList} = this;
-      if (recordList.length === 0) {return [];}
-      const newList = clone(recordList)
-        .filter(r => r.type === this.type)
-        .sort((a, b) => dayjs(b.recordTime).valueOf() - dayjs(a.recordTime).valueOf());
-      type Result = { title: string; total?: number; items: RecordItem[] }[]
-      const result: Result = [{title: dayjs(newList[0].recordTime).format('YYYY-MM-DD'), items: [newList[0]]}];
-      for (let i = 1; i < newList.length; i++) {
-        const current = newList[i];
-        const last = result[result.length - 1];
-        if (dayjs(last.title).isSame(dayjs(current.recordTime), 'day')) {
-          last.items.push(current);
-        } else {
-          result.push({title: dayjs(current.recordTime).format('YYYY-MM-DD'), items: [current]});
-        }
-      }
-      result.map(group => {
-        group.total = group.items.reduce((sum, item) => {
-          console.log(sum);
-          console.log(item);
-          return sum + item.amount;
-        }, 0);
-      });
-      return result;
+    const { recordList } = this;
+    const newList = clone(recordList)
+      .filter((r) => r.type === this.type)
+      .sort(
+        (a, b) => dayjs(b.recordTime).valueOf() - dayjs(a.recordTime).valueOf()
+      );
+    if (newList.length === 0) {
+      return [];
     }
-  tagString(tags: Tag[]){
-    return tags.length ===0? '无':tags.join(',')
+    type Result = { title: string; total?: number; items: RecordItem[] }[];
+    const result: Result = [
+      {
+        title: dayjs(newList[0].recordTime).format("YYYY-MM-DD"),
+        items: [newList[0]],
+      },
+    ];
+    for (let i = 1; i < newList.length; i++) {
+      const current = newList[i];
+      const last = result[result.length - 1];
+      if (dayjs(last.title).isSame(dayjs(current.recordTime), "day")) {
+        last.items.push(current);
+      } else {
+        result.push({
+          title: dayjs(current.recordTime).format("YYYY-MM-DD"),
+          items: [current],
+        });
+      }
+    }
+    result.map((group) => {
+      group.total = group.items.reduce((sum, item) => {
+        return sum + item.amount;
+      }, 0);
+    });
+    return result;
   }
-  beautify(string: string){
-    const day = dayjs(string)
-    console.log(day);
-    const now = dayjs()
-    if(day.isSame(now,"day")){
-      return "今天"
-    }else if(day.isSame(now.subtract( 1,"day"),"day")){
-      return "昨天"
-    }else if(day.isSame(now.subtract(2,"day"),"day")){
-      return "前天"
-    }else{
-      return string
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? "无" : tags.join("，");
+  }
+  beautify(string: string) {
+    const day = dayjs(string);
+    // console.log(day);
+    const now = dayjs();
+    if (day.isSame(now, "day")) {
+      return "今天";
+    } else if (day.isSame(now.subtract(1, "day"), "day")) {
+      return "昨天";
+    } else if (day.isSame(now.subtract(2, "day"), "day")) {
+      return "前天";
+    } else {
+      return string;
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.noResult{
+  padding: 16px;
+  text-align: center;
+}
 ::v-deep .type-tabs-item {
   background: white;
   &.selected {
@@ -103,21 +119,21 @@ export default class Statistics extends Vue {
     }
   }
 }
-%item{
-  padding:0 16px;
+%item {
+  padding: 0 16px;
   line-height: 40px;
   display: flex;
   justify-content: space-between;
   align-content: center;
 }
-.title{
-  @extend %item
+.title {
+  @extend %item;
 }
-.record{
+.record {
   @extend %item;
   background: white;
 }
-.notes{
+.notes {
   margin-right: atuo;
   margin-left: 8px;
 }
